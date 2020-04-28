22b536d14f78f7ce17171d20465cbde89e5f43e1
vectronic <hello.vectronic@gmail.com>
2020-04-28 11:16:34 +0100

    feat: initial vanilla version

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import builtins from 'rollup-plugin-node-builtins';
 import cleanup from 'rollup-plugin-cleanup';
 import commonjs from 'rollup-plugin-commonjs';
 import { eslint } from 'rollup-plugin-eslint';
@@ -40,41 +39,6 @@ module.exports = [
             }),
             commonjs(),
             resolve(),
-            cleanup({
-                extensions: ['ts']
-            })
-        ]
-    },
-    {
-        input: {
-            browser: 'src/index.ts'
-        },
-        output: {
-            dir: 'dist',
-            format: 'es',
-            sourcemap: true
-        },
-        watch: {
-            include: 'src/**',
-        },
-        plugins: [
-            peerDepsExternal(),
-            eslint({
-                include: [
-                    'src/**/*.ts'
-                ]
-            }),
-            typescript({
-                typescript: ts,
-                useTsconfigDeclarationDir: true,
-                cacheRoot: `${tempDir}/.rpt2_cache`
-            }),
-            commonjs(),
-            builtins(),
-            resolve({
-                browser: true,
-                preferBuiltins: false
-            }),
             cleanup({
                 extensions: ['ts']
             })
